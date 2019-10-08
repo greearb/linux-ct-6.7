aec4f2ed03c4241acdb203ae1f9ee69bb04d2e1f
Ben Greear <greearb@candelatech.com>
2019-10-08 14:53:09 -0700

    ath10k-ct: Fix 160Mhz radar flags.
    Fixes problems caused by previous mis-merge.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/mac.c
+++ drivers/net/wireless/ath/ath10k/mac.c
@@ -10739,8 +10739,6 @@ static struct ieee80211_iface_combination ath10k_10x_if_comb[] = {
 		.radar_detect_widths =	BIT(NL80211_CHAN_WIDTH_20_NOHT) |
 					BIT(NL80211_CHAN_WIDTH_20) |
 					BIT(NL80211_CHAN_WIDTH_40) |
-					BIT(NL80211_CHAN_WIDTH_80P80) | /* TODO:  Verify --Ben */
-					BIT(NL80211_CHAN_WIDTH_160) | /* TODO:  Verify --Ben */
 					BIT(NL80211_CHAN_WIDTH_80),
 #endif
 	},
@@ -10812,6 +10810,8 @@ static struct ieee80211_iface_combination ath10k_10x_ct_if_comb[] = {
 		.radar_detect_widths =	BIT(NL80211_CHAN_WIDTH_20_NOHT) |
 					BIT(NL80211_CHAN_WIDTH_20) |
 					BIT(NL80211_CHAN_WIDTH_40) |
+					BIT(NL80211_CHAN_WIDTH_80P80) | /* TODO:  Verify --Ben */
+					BIT(NL80211_CHAN_WIDTH_160) | /* TODO:  Verify --Ben */
 					BIT(NL80211_CHAN_WIDTH_80),
 #endif
 	},
@@ -10938,8 +10938,6 @@ static struct ieee80211_iface_combination ath10k_10_4_ct_if_comb[] = {
 		.radar_detect_widths =	BIT(NL80211_CHAN_WIDTH_20_NOHT) |
 					BIT(NL80211_CHAN_WIDTH_20) |
 					BIT(NL80211_CHAN_WIDTH_40) |
-					BIT(NL80211_CHAN_WIDTH_80P80) | /* TODO:  Verify --Ben */
-					BIT(NL80211_CHAN_WIDTH_160) | /* TODO:  Verify --Ben */
 					BIT(NL80211_CHAN_WIDTH_80),
 #endif
 	},
