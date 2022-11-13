625cd1dab9f5d10d53637cd63412fe6e1a550c98
Ben Greear <greearb@candelatech.com>
2022-11-12 16:18:46 -0800

    iwlwifi: RX signal improvements.
    Sum up multi-chain signals instead of just taking the maximum value.
    This gives more correct total on-air signal value.
    
    And, the firmware managed averages do not match expected per-packet values,
    so ignore those for MQ drivers and instead calculate averages in the driver
    and report that.
    
    Combined with the previous patch, this gives better looking RX signal report:
    
    	signal:  	-39 [-42, -41] dBm
    	signal avg:	-39 [-42, -41] dBm
    	beacon signal avg:	-46 dBm
    
    iwlwifi: Disable beacon filtering by default.
    
    So we can calculate the avg rx signal in the driver instead
    of depending on the firmware.
    
    iwlwifi: Fix per-chain rssi averaging for 1nss frames.
    
    Averaging in the weak secondary chain for a single-spatial-stream packet
    causes the per-chain averages to look abnormally weak.  So, move calculation
    of nss above where the rssi is calculated, and then use the nss inside the
    rssi-calculation to ignore the weak spatial stream for 1nss frames.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -2723,4 +2723,38 @@ bool iwl_mvm_enable_fils(struct iwl_mvm *mvm,
 			 struct ieee80211_chanctx_conf *ctx);
 void iwl_mvm_mld_select_links(struct iwl_mvm *mvm, struct ieee80211_vif *vif,
 			      bool valid_links_changed);
+
+static inline int iwl_mvm_sum_sigs_2(int a, int b)
+{
+       int diff;
+
+       /* S8_MIN means value-is-not-set */
+       if (b == S8_MIN)
+               return a;
+       if (a == S8_MIN)
+               return b;
+
+       if (a >= b) {
+               /* a is largest value, add to it. */
+               diff = a - b;
+               if (diff == 0)
+                       return a + 3;
+               else if (diff <= 2)
+                       return a + 2;
+               else if (diff <= 6)
+                       return a + 1;
+               return a;
+       } else {
+               /* b is largest value, add to it. */
+               diff = b - a;
+               if (diff == 0)
+                       return b + 3;
+               else if (diff <= 2)
+                       return b + 2;
+               else if (diff <= 6)
+                       return b + 1;
+               return b;
+       }
+}
+
 #endif /* __IWL_MVM_H__ */
